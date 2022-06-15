6345ce5c2ace4088dc9a81776693be561208481a
Raghavan alias Saravanan M <raghavan.muthu@outlook.com>
2022-06-16 02:09:37 +0530

    V2.1 - VersionHistory.php - case sensitive file - added with right case in filename

---
+++ versionHistory.php
@@ -0,0 +1,129 @@
+<?php
+  //require_once 'inc/header.php';
+  require_once __DIR__ . '/inc/header.php';
+?>
+
+<h2>Completed</h2>
+
+<table class="table table-hover table-bordered versionHistory">
+    <thead>
+        <tr>
+            <th>Version</th>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Remarks</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.0</td>
+            <td>22 Apr 2022 Friday</td>
+            <td>
+                <li>Student Module - Add User, View Profiles</li>
+                <li>Listing with Pagination</li>
+                <li>Profile Pic enabled</li>
+                <li>Session Timeout</li>
+              </ul>
+            </td>
+            <td>
+              <ul>
+                <li>Tested in PROD</li>
+              </ul>
+            </td>
+        </tr>
+        <tr>
+            <td>2.0</td>
+            <td>24 Apr 2022 Friday</td>
+            <td>
+                <li>Staff Entitlements are in place</li>
+              </ul>
+            </td>
+            <td>
+              <ul>
+                <li>Tested in PROD</li>
+              </ul>
+            </td>
+        </tr>
+        <tr>
+            <td>2.1</td>
+            <td>15 Jun 2022 Wednesday</td>
+            <td>
+                <li>Campus Pic and Logo are in the Home Page</li>
+              </ul>
+            </td>
+            <td>
+              <ul>
+                <li>Tested in PROD</li>
+              </ul>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+<h2>Known Issues</h2>
+<table class="table table-hover table-bordered versionHistory">
+    <thead>
+        <tr>
+            <th>Version</th>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Remarks</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.0</td>
+            <td>19 Apr 2022 Tuesday</td>
+            <td>
+              <ul>
+                <li>Profile Pic should have an extension of <code>.jpg</code> only then it gets displayed properly.</li>
+              </ul>
+            </td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+
+<h2>What's Next</h2>
+
+<table class="table table-hover table-bordered versionHistory">
+    <thead>
+        <tr>
+            <th>Version</th>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Remarks</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.1</td>
+            <td>22 Apr 2022 Friday</td>
+            <td>
+              <ul>
+                <li>Staff Entitlements to be in place</li>
+                <li>Bug fix - Profile Pic extension</li>
+                <li>Secured URLs</li>
+              </ul>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>2.0</td>
+            <td>22 Apr 2022 Friday</td>
+            <td>
+              <ul>
+                <li>Staff Entitlements to be in place</li>
+                <li>Bug fix - Profile Pic extension</li>
+                <li>Search features on both List Users and List Students</li>
+                <li>College Logo to be placed on the Landing Page and on the Banner on top</li>
+              </ul>
+            </td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+<?php
+  //require_once 'inc/footer.php';
+  require_once __DIR__ . '/inc/footer.php';
+?>
